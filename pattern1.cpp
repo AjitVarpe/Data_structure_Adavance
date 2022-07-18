fdba23da504b41b8b92a3f6b085fc6040a5c9191
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-07-18 14:32:02 +0530

    inserted new pattern

---
+++ pattern1.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#include<limits>
+#include<String>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    while (i<=n)
+    {
+        int j=1;
+        while (j<=n)
+        {
+            cout<<i;
+            j++;
+        }
+        cout<<endl;
+        i++;
+
+        
+    }
+    
+
+
+    return 0;
+}
