856793b2eb37014e9dfb2e9aadaf4cb89da2e5bf
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-08-16 20:19:03 +0530

    vector code added

--- stlvector.cpp
+++ stlvector.cpp
@@ -5,10 +5,21 @@ int main(){
     
 
     // declaring vector of 4 size 
-    vector<int> a(5,1);
-    for (int i:a)
+    vector<int> a;
+    // insert element in vector
+    cout<<"Enter how many element we want to insert "<<endl;
+    int size,element;
+    cin>>size;
+    for (size_t i = 0; i < size; i++)
     {
-        cout<<i<<" ";
+        cin>>element;
+        a.push_back(element);
+    }
+
+    // Display element 
+    for (size_t i = 0; i < size; i++)
+    {
+        cout<<a.at(i)<<" ";
     }
     
 
