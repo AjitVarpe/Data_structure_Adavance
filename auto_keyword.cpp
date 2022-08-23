a8001012d935f0b96de8cf62a7ce1c16c88ee37d
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-08-23 15:29:00 +0530

    auto keyword code

---
+++ auto_keyword.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<limits>
+#include<String>
+using namespace std;
+
+int main(){
+    auto x = 50;
+    auto d ="string";
+    // cin>>d;
+    // cout<<"data type of x is "<<typeof(x);
+    cout<<endl<<d;
+
+
+    return 0;
+}
