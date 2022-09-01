21a6eb074f00e3ff3ea5af55d20a410efc37eeed
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-09-01 16:35:48 +0530

    stack codes

--- STL_stack.cpp
+++ STL_stack.cpp
@@ -4,18 +4,44 @@
 using namespace std;
 int main(){
     stack<int> st;
+    
+    stack<int> stl;
+    stl.push(1);
+    stl.push(3);
+    stl.push(4);
+    stl.push(5);
+    stl.push(6);
+    stl.push(7);
     st.push(10);
     st.push(80);
     st.push(50);
     st.push(40);
     st.push(30);
     st.push(20);
+
+    cout<<"size of the stack : ";
+    cout<<st.size()<<endl;
+
+    // check stack is empty or not 
+    cout<<st.empty()<<endl;
+
     cout<<"Stack Variables : "<<endl;
+    // loop will work util the stack having element in it
+    stl.swap(st);
+     while (!stl.empty())
+    {
+        cout<<stl.top()<<" ";
+        stl.pop();
+    }
+    cout<<"Reversed stack is "<<endl;
     while (!st.empty())
     {
         cout<<st.top()<<" ";
         st.pop();
     }
+    
+
+    
     
     
     return 0;
