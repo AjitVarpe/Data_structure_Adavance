ce0c361f24a3d5ea61a3202d8854d05a3e75e17f
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-09-18 12:34:17 +0530

    insert new time complexity code

---
+++ timecoplex.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#include<limits>
+#include<String>
+using namespace std;
+
+int main(){
+    int count = 5;
+    for (int i = 0; i < count; i++)
+    {
+        for (auto j = 0; j < count; j++)
+        {
+            cout<<i<<" "<<j<<endl;
+        }
+        
+    }
+    
+
+
+    return 0;
+}
