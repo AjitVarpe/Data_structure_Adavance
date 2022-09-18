26b93324d19d9ecec91209e76aa6db8a654ed09d
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-09-18 14:14:53 +0530

    changes made in selecting mid condition where  mid  = start + (end- start)/2;

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,27 +1,35 @@
-#include<bits/stdc++.h>
-#include<limits>
-#include<String>
+#include <bits/stdc++.h>
+#include <limits>
+#include <String>
 using namespace std;
-int binarySearch(int arr[],int size,int element){
-    int start = 0, end =size-1;
-    while(start<=end){
-        int mid = (start + end) /2;
-        if(arr[mid]==element)
-        return mid;
-        if(arr[mid]>element) end = mid -1;
+int binarySearch(int arr[], int size, int element)
+{
+    int start = 0, end = size - 1;
+    while (start <= end)
+    {
+        
+        
+        // this condition will overflow when we r having 2^31-1 size of index in start and end so we cant store the max element in int
+        // int mid = (start + end) / 2;
+
+        //  So the Smartest Move will be the 
+        int mid = start + (end -start)/2;
+
+        if (arr[mid] == element)
+            return mid;
+        if (arr[mid] > element)
+            end = mid - 1;
         else
-        start = mid +1;
+            start = mid + 1;
     }
     return -1;
 }
-int main(){
-    int oddarray[6] = {1,2 ,3 ,4,5,6};
-    int evenarray[6] = {10,20,30,40,50,60};
+int main()
+{
+    int oddarray[6] = {1, 2, 3, 4, 5, 6};
+    int evenarray[6] = {10, 20, 30, 40, 50, 60};
 
-    cout<<" the Index of the 5 element is "<<binarySearch(oddarray,6,5)<<endl;
-
-    
-
+    cout << " the Index of the 5 element is " << binarySearch(oddarray, 6, 5) << endl;
 
     return 0;
 }
