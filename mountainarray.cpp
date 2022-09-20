f92c1a9a4261bff52578a23b8c270b985d15ab59
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-09-20 11:03:25 +0530

    inserted new leetccode solution to the production

---
+++ mountainarray.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include<limits>
+#include<String>
+using namespace std;
+    bool validMountainArray(vector<int>& arr) {
+     int s=0,e=arr.si   
+    }
+int main(){
+    
+
+
+    return 0;
+}
