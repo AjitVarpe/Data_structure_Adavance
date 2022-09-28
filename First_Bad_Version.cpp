2d4ddbf8e6ffa246a41cbc594f2618e33d2d4408
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-09-28 11:43:30 +0530

    209. Minimum Size Subarray Sum

--- First_Bad_Version.cpp
+++ First_Bad_Version.cpp
@@ -3,6 +3,8 @@
 // bool isBadVersion(int version);
 
 class Solution {
+
+    // the time complexity is o(n) here 
 public:
     int firstBadVersion(int n) {
      for(int i=1;i<=n;i++){
@@ -11,5 +13,29 @@ public:
          } 
      }
         return -1;
+    }
+};
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+
+// the time complexity is o(log n ) where the binary search approch is used to solve the problem in 
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+     int s = 0 ,e = n,bad;
+        int mid = s + (e-s)/2;
+        while(s<=e){
+            if(isBadVersion(mid)){
+                bad = mid;
+                e = mid - 1;
+            }else{
+                s = mid +1;
+            }
+            mid = s + (e-s)/2;
+        }
+        return bad;
     }
 };
