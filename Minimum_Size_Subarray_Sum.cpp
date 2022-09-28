5928876b5f30f12367d23981833332fa9b54f56b
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-09-28 11:46:39 +0530

    209. Minimum Size Subarray Sum

--- Minimum_Size_Subarray_Sum.cpp
+++ Minimum_Size_Subarray_Sum.cpp
@@ -13,3 +13,10 @@ public:
         return ans%(nums.size()+1);
     }
 };
+
+// the above code contain the sliding window approch to solve the problem of give question type
+
+
+// for ex the array consist of int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+// so the sliding window apporch uses the approch the window will slide from 1 2 3 4 to next slide to 2 3 4 5 to next slide 3 4 5 6
+ 
