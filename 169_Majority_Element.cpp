5f2662ba391696fd2d513886ae5b52105f2005f3
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-10-30 20:57:42 +0530

    169. Majority Element

---
+++ 169_Majority_Element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        return nums[(nums.size()/2)];
+    }
+};
+
+#include<bits/stdc++.h>
+#include<limits>
+#include<String>
+using namespace std;
+
+int main(){
+    
+
+
+    return 0;
+}
