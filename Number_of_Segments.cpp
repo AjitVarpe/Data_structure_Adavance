cd38b7a0168f786e5927795cfe17c47987a28ff0
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-11-02 14:22:11 +0530

    Number of Segments

---
+++ Number_of_Segments.cpp
@@ -0,0 +1,24 @@
+// Number of Segments
+
+class Solution {
+public:
+       int countSegments(string s) {
+        stringstream ss(s);
+        string str;
+        int count=0;
+        while(ss>>str) count++;
+        return count;
+    }
+};
+
+#include<bits/stdc++.h>
+#include<limits>
+#include<String>
+using namespace std;
+
+int main(){
+    
+
+
+    return 0;
+}
