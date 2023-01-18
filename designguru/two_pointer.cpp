81767ebf0d4fcf232d2006299f3e1a107972ee71
Ajit Varpe <ajitvarpe8652@gmail.com>
2023-01-18 13:28:27 +0530

    improvement

--- designguru/two_pointer.cpp
+++ designguru/two_pointer.cpp
@@ -25,3 +25,19 @@ int main(){
     
     return 0;
 }
+
+// Alternative Approch for the same Two pointer Approch 
+class PairWithTargetSum {
+ public:
+  static pair<int, int> search(const vector<int>& arr, int targetSum) {
+    unordered_map<int, int> nums;  // to store number and its index
+    for (int i = 0; i < arr.size(); i++) {
+      if (nums.find(targetSum - arr[i]) != nums.end()) {
+        return make_pair(nums[targetSum - arr[i]], i);
+      } else {
+        nums[arr[i]] = i;  // put the number and its index in the map
+      }
+    }
+    return make_pair(-1, -1);  // pair not found
+  }
+};
